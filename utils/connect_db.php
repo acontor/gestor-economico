2adca275bc99d300aaaae1bff91f6dd5932b150b
Alvaro Conde Torres <alvaroconde03@gmail.com>
2020-02-12 11:39:00 +0100

    Tarea Online DWES 3 - Ejercicio 3

--- utils/connect_db.php
+++ utils/connect_db.php
@@ -1,9 +1,6 @@
 <?php
-$host = '';
-$dbname = '';
-$costumername = '';
-$password = '';
-// MySQL/MariaDB
-// $dbh = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $costumername, $password);
-// PostgreSQL
-$dbh = new PDO('pgsql:host=' . $host . ';dbname=' . $dbname, $costumername, $password);
+$host = 'ec2-46-137-177-160.eu-west-1.compute.amazonaws.com';
+$dbname = 'deod10l4p8m3cq';
+$user = 'qrwqmcubwdjfvd';
+$password = '1f2b60998e9c2403511f5c2f5e684e8ddbacff9be39d996a23e03debc4400643';
+$dbh = new PDO('pgsql:host=' . $host . ';dbname=' . $dbname, $user, $password);
